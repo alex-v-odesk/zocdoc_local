cdbcf6db9803b976129b31cb28dd08b49365c1ba
Alana Farkas <alana.farkas@zocdoc.com>
2017-11-28 12:47:58 -0500

    loads all and compiles new styles

--- wp-content/themes/page-careers.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //JOB DETAILS
-    if(isset($wp_query->query_vars['job_id'])) {
-        get_template_part('template-parts/careers','header');
-        get_template_part('template-parts/job', 'details');
-    } else {
-        get_template_part('index');
-    }
-?>
--- wp-content/themes/template-parts/legacy-header.php
+++
@@ -1,5 +0,0 @@
-<header id="page-header" class="page-header healthsystems <?php if (strpos($_SERVER['REQUEST_URI'], "job")): ?>job-detail<?php endif; ?>" role="banner">
-
-    <h1 class="logo"><a href="https://www.zocdoc.com/"><strong>Zocdoc</strong></a></h1>
-
-</header>
